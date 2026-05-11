fb7f813adf6b0c1b4f590afd46c8b9a5699ede65
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2026-05-11 18:39:52 +0200

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router";
+import { Layout } from "./Layout";
+
 function App() {
-  return <section></section>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes for authUser */}
+        <Route index element={<p>Login</p>} />
+        <Route path="register" element={<p>register</p>} />
+        {/* public routes  */}
+        <Route path="news" element={<p>news</p>} />
+        <Route path="notices" element={<p>notices</p>} />
+        <Route path="friends" element={<p>friends</p>} />
+        {/* private routes */}
+        {/* <Route path="profile" element={<p>user profile</p>} /> */}
+      </Route>
+
+      {/* public routes */}
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Layout.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router";
+
+export const Layout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>News</li>
+            <li>Find pet</li>
+            <li>Our friends</li>
+          </ul>
+        </nav>
+        <div>
+          <button>Log in</button>
+
+          <button>Registration</button>
+        </div>
+      </header>
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
