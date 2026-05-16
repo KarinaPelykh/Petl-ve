fbb92563884c143b8c22a2bd0032879bc97aa50a
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2026-05-16 21:32:28 +0200

    feat: registration form

---
+++ src/feature/register/ui/Registarion.tsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router";
+import { Button } from "../../../shared/ui/Button";
+import {
+  Form,
+  FromField,
+  Input,
+  Label,
+  MessageText,
+} from "../../../shared/ui/Form";
+import { Heading } from "../../../shared/ui/Heading";
+
+export const RegistrationForm = () => {
+  return (
+    <Form className="py-15 tablet-l:rounded-m tablet-l:py-17.75 tablet-l:px-35 desktop-l:px-21 desktop-l:py-29.5">
+      <Heading className="tablet-l:text-5xl tablet-l:mb-4" as="h1">
+        Register
+      </Heading>
+      <p className="text-ms mb-6 tablet-l:text-m desktop-l:mb-8">
+        Thank you for your interest in our platform.
+      </p>
+      <FromField name="name">
+        <Label htmlFor="name" />
+        <Input type="text" placeholder="Name" />
+        <MessageText />
+      </FromField>
+      <FromField name="email">
+        <Label htmlFor="email" />
+        <Input type="text" placeholder="Email" />
+        <MessageText />
+      </FromField>
+      <FromField name="password">
+        <Label htmlFor="password" />
+        <Input type="password" placeholder="Password" />
+        <MessageText />
+      </FromField>
+      <FromField name="confirmPassword">
+        <Label htmlFor="confirmPassword" />
+        <Input type="password" placeholder="Confirm password" />
+        <MessageText />
+      </FromField>
+      <Button className="bg-yellow w-full text-white uppercase mb-3 tablet-l:mt-16 desktop-l:py-4">
+        Registration
+      </Button>
+      <p className="text-black/50 text-s text-center  tablet-l:text-ms">
+        Already have an account?
+        <Link className="text-yellow underline cursor-pointer" to="/login">
+          Login
+        </Link>
+      </p>
+    </Form>
+  );
+};
