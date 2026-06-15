f0b92a2a23e157a3343738e2f8ff3228032d73bd
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2026-06-15 16:45:15 +0200

    feat: hover for navigation

--- src/widget/header/HeaderNavList.tsx
+++ src/widget/header/HeaderNavList.tsx
@@ -19,12 +19,15 @@ export const HeaderNavList = ({
   return (
     <ul className={clsx(className, "gap-2")}>
       {nav.map((item) => (
-        <Link key={item.name} to={item.link}>
+        <Link key={item.name} to={item.link} className={clsx()}>
           <li
             className={clsx(
               "rounded-ms text-ms hover:border-yellow flex w-fit items-center border border-black/15 px-5 py-3.75 shadow-lg transition-all duration-500 hover:scale-105",
               itemClassName,
               homePage && "border-white text-white",
+              !homePage &&
+                location.pathname === `/${item.link}` &&
+                "border-yellow",
             )}
           >
             {item.name}
