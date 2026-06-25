aa06809d7058e29a64fc32ec4db6e7a8913f9990
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2026-06-25 16:41:58 +0200

    feat: add redux-persist

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,17 +1,50 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { noticesReducer } from "../../shared/api/redux/notices/slice";
 import { friendsReducer } from "../../shared/api/redux/friends/slice";
 import { newsReducer } from "../../shared/api/redux/news/slice";
 import { authReducer } from "../../shared/api/redux/user/slice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const str =
+  typeof storage === "object" && storage !== null && storage.__esModule
+    ? storage.default
+    : storage;
+
+const persistConfig = {
+  key: "root",
+  storage: str,
+  whitelist: ["auth", "notices", "news", "friends"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  notices: noticesReducer,
+  friends: friendsReducer,
+  news: newsReducer,
+});
+console.log("storage =", storage);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    notices: noticesReducer,
-    friends: friendsReducer,
-    news: newsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
-import { store } from "./app/store/store.ts";
-
+import { persistor, store } from "./app/store/store.ts";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Missing type declarations for redux-persist integration react
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
 );
